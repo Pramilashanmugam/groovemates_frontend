6adad21381c1bc33978e41d45869c9ea95eb29c9
Pramila Shanmugam <pramila.sl@gmail.com>
2024-11-14 16:55:22 +0000

    api/axiosDefaults- update intercept for request and response, contexts/currentusercontext - update usememo

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://groovemates-backend-b16861eb6026.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create(); // intercept the request
+export const axiosRes = axios.create(); // intercept the response
