6791dc3e87a202ca826aae30b4a8c4c92100fcd7
Pramila Shanmugam <pramila.sl@gmail.com>
2024-11-17 20:58:24 +0000

    postcreateform empty line removed

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -8,7 +8,6 @@ import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
 import Image from "react-bootstrap/Image";
 import Upload from "../../assets/upload.png";
-
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
