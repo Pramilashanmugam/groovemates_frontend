3e35c1c53a946f6d39223b2e10bf23d841a03c1e
Pramila Shanmugam <pramila.sl@gmail.com>
2024-11-18 22:34:07 +0000

    complete revamp of code done, removed replies from comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Media } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+import styles from "../../styles/Comment.module.css";
 
-function Comment({ id, owner, description, created_at, profile_image, replies }) {
+const Comment = (props) => {
+  const { profile_id, profile_image, owner, updated_at, description } = props;
+
   return (
-    <div key={id} style={{ marginBottom: "15px" }}>
-      <div className="comment-header">
-        <img src={profile_image} alt={owner} style={{ width: "30px", height: "30px", borderRadius: "50%" }} />
-        <strong>{owner}</strong>
-      </div>
-      <p>{description}</p>
-      <small>{created_at}</small>
-
-      {replies && replies.length > 0 && (
-        <div style={{ marginLeft: "20px" }}>
-          <h5>Replies:</h5>
-          {replies.map((reply) => (
-            <Comment key={reply.id} {...reply} />
-          ))}
-        </div>
-      )}
+    <div>
+      <hr />
+      <Media>
+        <Link to={`/profiles/${profile_id}`}>
+          <Avatar src={profile_image} />
+        </Link>
+        <Media.Body className="align-self-center ml-2">
+          <span className={styles.Owner}>{owner}</span>
+          <span className={styles.Date}>{updated_at}</span>
+          <p>{description}</p>
+        </Media.Body>
+      </Media>
     </div>
   );
-}
+};
 
 export default Comment;
