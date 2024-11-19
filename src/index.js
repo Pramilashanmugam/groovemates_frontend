7bab38a6081b0afed828154313a7d27e49dfd3fb
Pramila Shanmugam <pramila.sl@gmail.com>
2024-11-19 21:35:09 +0000

    typo error on Profiledatacontext updated

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
-import { ProfileDataProvider } from "./contexts/ProfileDataProvider";
+import { ProfileDataProvider } from "./contexts/ProfileDataContext";
 
 ReactDOM.render(
   <React.StrictMode>
