f568eb4568a1a9857f5eededb00f2448b99a2dc0
Pramila Shanmugam <pramila.sl@gmail.com>
2024-11-29 16:31:18 +0000

    navlink for sharedpost updated in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -51,6 +51,13 @@ const NavBar = () => {
       >
         <i className="fas fa-heart"></i>Liked
       </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/shared"
+      >
+        <i className="fa-solid fa-share"></i>SharedPosts
+      </NavLink>
       <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
         <i className="fas fa-sign-out-alt"></i>Sign out
       </NavLink>
