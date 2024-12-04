322a1052b598809d89bc8e23c0f48abe7e2c3321
Pramila Shanmugam <pramila.sl@gmail.com>
2024-12-04 09:51:58 +0000

    removed commented out line and updated comments

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://groovemates-backend-b16861eb6026.herokuapp.com/'
-//axios.defaults.baseURL = 'https://8000-pramilashan-groovemates-eommjup5mkz.ws.codeinstitute-ide.net/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+// Set up global default configuration for axios requests
+axios.defaults.baseURL =
+  "https://groovemates-backend-b16861eb6026.herokuapp.com/"; // The base URL for all requests
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data"; // Set default content type for POST requests (for file uploads)
+axios.defaults.withCredentials = true; // Ensure that cookies (including authentication cookies) are sent with requests
 
-export const axiosReq = axios.create(); // intercept the request
-export const axiosRes = axios.create(); // intercept the response
+// Create a custom instance for request interception
+export const axiosReq = axios.create(); // This instance can be used to intercept requests
+
+// Create a custom instance for response interception
+export const axiosRes = axios.create(); // This instance can be used to intercept responses
